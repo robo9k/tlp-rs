af30017660aafb53703a1f94899c3f6d0779c110
robot9001 <robo9k@symlink.io>
2026-04-27 17:35:38 +0200

    Implement `serde` feature with `serde_core` and `serde_test2`
    Signed-off-by: robot9001 <robo9k@symlink.io>

--- src/lib.rs
+++ src/lib.rs
@@ -127,9 +127,9 @@ impl core::error::Error for ParseLabelError {}
 #[cfg(feature = "serde")]
 mod serde {
     use crate::Label;
-    use ::serde::de::{self, Deserialize, Deserializer, MapAccess, SeqAccess, Visitor};
-    use ::serde::ser::{Serialize, SerializeStruct, Serializer};
-    use core::fmt::Write;
+
+    use serde_core::de::{Deserialize, Deserializer};
+    use serde_core::ser::{Serialize, Serializer};
 
     #[cfg_attr(docsrs, doc(cfg(feature = "serde")))]
     impl Serialize for Label {
@@ -137,7 +137,24 @@ mod serde {
         where
             S: Serializer,
         {
-            todo!()
+            if serializer.is_human_readable() {
+                match self {
+                    Label::Red => serializer.serialize_str(Label::RED),
+                    Label::AmberStrict => serializer.serialize_str(Label::AMBER_STRICT),
+                    Label::Amber => serializer.serialize_str(Label::AMBER),
+                    Label::Green => serializer.serialize_str(Label::GREEN),
+                    Label::Clear => serializer.serialize_str(Label::CLEAR),
+                }
+            } else {
+                const NAME: &str = "Label";
+                match self {
+                    Label::Red => serializer.serialize_unit_variant(NAME, 0, "Red"),
+                    Label::AmberStrict => serializer.serialize_unit_variant(NAME, 1, "AmberStrict"),
+                    Label::Amber => serializer.serialize_unit_variant(NAME, 2, "Amber"),
+                    Label::Green => serializer.serialize_unit_variant(NAME, 3, "Green"),
+                    Label::Clear => serializer.serialize_unit_variant(NAME, 4, "Clear"),
+                }
+            }
         }
     }
 
@@ -147,15 +164,177 @@ mod serde {
         where
             D: Deserializer<'de>,
         {
-            todo!()
+            use serde_core::de::{EnumAccess, VariantAccess as _, Visitor};
+            use serde_core::de::{Error, Unexpected};
+
+            const NAME: &str = "Label";
+
+            const VARIANTS: &[&str] = &["Red", "AmberStrict", "Amber", "Green", "Clear"];
+
+            enum LabelVariant {
+                Red,
+                AmberStrict,
+                Amber,
+                Green,
+                Clear,
+            }
+
+            struct LabelVariantVisitor;
+
+            impl<'de> Visitor<'de> for LabelVariantVisitor {
+                type Value = LabelVariant;
+
+                fn expecting(&self, formatter: &mut core::fmt::Formatter) -> core::fmt::Result {
+                    formatter.write_str("Label variant identifier")
+                }
+
+                fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+                where
+                    E: Error,
+                {
+                    match v {
+                        0 => Ok(LabelVariant::Red),
+                        1 => Ok(LabelVariant::AmberStrict),
+                        2 => Ok(LabelVariant::Amber),
+                        3 => Ok(LabelVariant::Green),
+                        4 => Ok(LabelVariant::Clear),
+
+                        _ => Err(Error::invalid_value(
+                            Unexpected::Unsigned(v),
+                            &"variant index 0 <= i < 5",
+                        )),
+                    }
+                }
+
+                fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+                where
+                    E: Error,
+                {
+                    match v {
+                        "Red" => Ok(LabelVariant::Red),
+                        "AmberStrict" => Ok(LabelVariant::AmberStrict),
+                        "Amber" => Ok(LabelVariant::Amber),
+                        "Green" => Ok(LabelVariant::Green),
+                        "Clear" => Ok(LabelVariant::Clear),
+
+                        _ => Err(Error::unknown_variant(v, VARIANTS)),
+                    }
+                }
+            }
+
+            impl<'de> Deserialize<'de> for LabelVariant {
+                fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+                where
+                    D: Deserializer<'de>,
+                {
+                    deserializer.deserialize_identifier(LabelVariantVisitor)
+                }
+            }
+
+            struct LabelVisitor;
+
+            impl<'de> Visitor<'de> for LabelVisitor {
+                type Value = Label;
+
+                fn expecting(&self, formatter: &mut core::fmt::Formatter) -> core::fmt::Result {
+                    formatter.write_str("enum Label")
+                }
+
+                fn visit_enum<A>(self, data: A) -> Result<Self::Value, A::Error>
+                where
+                    A: EnumAccess<'de>,
+                {
+                    match data.variant()? {
+                        (LabelVariant::Red, variant) => {
+                            variant.unit_variant()?;
+                            Ok(Label::Red)
+                        }
+                        (LabelVariant::AmberStrict, variant) => {
+                            variant.unit_variant()?;
+                            Ok(Label::AmberStrict)
+                        }
+                        (LabelVariant::Amber, variant) => {
+                            variant.unit_variant()?;
+                            Ok(Label::Amber)
+                        }
+                        (LabelVariant::Green, variant) => {
+                            variant.unit_variant()?;
+                            Ok(Label::Green)
+                        }
+                        (LabelVariant::Clear, variant) => {
+                            variant.unit_variant()?;
+                            Ok(Label::Clear)
+                        }
+                    }
+                }
+
+                fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+                where
+                    E: Error,
+                {
+                    match v {
+                        Label::RED => Ok(Label::Red),
+                        Label::AMBER_STRICT => Ok(Label::AmberStrict),
+                        Label::AMBER => Ok(Label::Amber),
+                        Label::GREEN => Ok(Label::Green),
+                        Label::CLEAR => Ok(Label::Clear),
+
+                        _ => Err(Error::invalid_value(
+                            Unexpected::Str(v),
+                            &"one of \"TLP:RED\", \"TLP:AMBER+STRICT\", \"TLP:AMBER\", \"TLP:GREEN\", \"TLP:CLEAR\"",
+                        )),
+                    }
+                }
+            }
+
+            if deserializer.is_human_readable() {
+                deserializer.deserialize_str(LabelVisitor)
+            } else {
+                deserializer.deserialize_enum(NAME, VARIANTS, LabelVisitor)
+            }
         }
     }
 
     #[cfg(test)]
     mod tests {
-        // TODO: test_serialize
-        // TODO: test_deserialize
-        // TODO: test_roundtrip
+        use super::*;
+
+        use serde_test2::{
+            Compact, Configure as _, Readable, Token, assert_de_tokens_error, assert_tokens,
+        };
+
+        #[test]
+        fn ser_de() {
+            let label = Label::AmberStrict;
+
+            assert_tokens(
+                &label.compact(),
+                &[Token::UnitVariant {
+                    name: "Label",
+                    variant_index: 1,
+                    variant: "AmberStrict",
+                }],
+            );
+
+            assert_tokens(&label.readable(), &[Token::Str("TLP:AMBER+STRICT")]);
+        }
+
+        #[test]
+        fn de_error() {
+            assert_de_tokens_error::<Compact<Label>>(
+                &[Token::UnitVariant {
+                    name: "Label",
+                    variant_index: 5,
+                    variant: "hurz",
+                }],
+                "invalid value: unit variant, expected one of `Red`, `AmberStrict`, `Amber`, `Green`, `Clear`",
+            );
+
+            assert_de_tokens_error::<Readable<Label>>(
+                &[Token::Str("hurz")],
+                "invalid value: string \"hurz\", expected one of \"TLP:RED\", \"TLP:AMBER+STRICT\", \"TLP:AMBER\", \"TLP:GREEN\", \"TLP:CLEAR\"",
+            );
+        }
     }
 }
 
